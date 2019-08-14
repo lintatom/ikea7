1d277985a2bb54b69a2833da882328860608b894
lintatom <34221141+lintatom@users.noreply.github.com>
2019-08-14 14:22:18 -0500

    test
    test

--- src/test/java/com/example/tests/MyIkeaTest.java
+++ src/test/java/com/example/tests/MyIkeaTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class MyIkea {
+public class MyIkeaTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
